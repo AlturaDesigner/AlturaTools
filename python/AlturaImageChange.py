e52a188692f471d3011d66eb76809086c9bf823a
Designer <56726555+AlturaDesigner@users.noreply.github.com>
2022-08-01 22:10:38 +0200

    Update AlturaImageChange.py

--- python/AlturaImageChange.py
+++ python/AlturaImageChange.py
@@ -1,9 +1,9 @@
 import requests
 
-url = 'https://api.alturanft.com/api/v2/item/update_primary_image?apiKey=XXXX'
+url = 'https://api.alturanft.com/api/v2/item/update_primary_image?apiKey=XXXX' # Replace XXXX With Api key from Altura Developer portal https://developer.alturanft.com/
 myobj = {'tokenId': 1, 
-                'address': '0x4ddee11d87a535ec71817b558d81bda24f4cac7b',
-                'imageIndex': 1}
+                'address': '0x4ddee11d87a535ec71817b558d81bda24f4cac7b', # Replace with address of your collection
+                'imageIndex': 1} # image index  starts with 0
 
 x = requests.post(url, data = myobj)
 
