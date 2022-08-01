4c6c1416da933ee0ee57c9ae5709b7d71289bbe2
Designer <56726555+AlturaDesigner@users.noreply.github.com>
2022-08-01 22:21:19 +0200

    Create PropertyAutoDecrement.py

---
+++ python/PropertyAutoDecrement.py
@@ -0,0 +1,24 @@
+
+import requests
+import json
+import schedule
+import ftplib
+
+response = requests.get('https://app.alturanft.com/api/external/item?apiKey=XXXX&collectionId=0xf3b083f847b9b49af7e9a42b35e2ae9ebe5960df')
+        test = response.json()
+
+        print(test)
+
+        with open("data.json", "w") as outfile:
+            json.dump(test, outfile)
+            print("Data Ready.")
+            
+token = test['items'][vario]['tokenId']
+print(token)
+
+url = 'https://api.alturanft.com/api/v2/item/update_property?apiKey=XXXX'
+                myobj = {'tokenId':token,             'address': '0xf3b083f847b9b49af7e9a42b35e2ae9ebe5960df',
+                                'propertyValue': int(token) - 1}
+
+
+                x = requests.post(url, data = myobj)
